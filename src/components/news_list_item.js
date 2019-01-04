8ba19b5db57ee7fffe9e7921e1cf2e772fa1d88b
Mamithi <19lawrence93@gmail.com>
2019-01-04 17:14:02 +0300

    Stuff

---
+++ src/components/news_list_item.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from '../css/styles.css'
+
+const NewsItem = ({item}) => {
+    return (
+        <div className={styles.news_item}>
+        <h3>{item.title}</h3>
+        <div>
+            {item.feed}
+        </div>
+    </div>
+    )
+}
+
+export default NewsItem;
+
